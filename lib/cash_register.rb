a830b519352c32451e7c248d2b7d56abb7e37d6e
Evan Candler <emcandler21@gmail.com>
2018-03-01 05:24:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount, :previous_total, :items
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @previous_total = 0
+    @items = []
+  end
 
+def add_item
+  self.total += price * quantity
+  quantity.times do
+    @items << title
+  end
+end
